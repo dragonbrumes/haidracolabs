e03865f74f4a062e2a552f9a1252f079440ff587
Stéphane aal <dragonbrumes@gmail.com>
2023-01-02 19:55:10 +0100

    First commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import logo from "../home_welcome.png";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <main className="App-header">
+        <img src={logo} className="logo" alt="logo" />
+      </main> 
+    </div>
+  );
+}
---
+++ src/pages/PrivacyApp.js
@@ -0,0 +1,205 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+export default function PrivacyApp() {
+  return (
+    <Container id="main" className="content-wrap" role="main">
+        <header className="entry-header">
+          <h2 className="entry-title">Privacy Policy for our Apps</h2>{" "}
+        </header>
+
+        <div className="entry-content">
+          <p>
+            This Privacy Policy outlines the practices of Hai Draco Labs (the
+            &#8220;Company&#8221;, &#8221; we&#8221;, &#8220;us&#8221; or
+            &#8220;our&#8221;) concerning the collection, use and disclosure of
+            information pertaining to end users of our apps.
+          </p>
+          <p>
+            This Privacy Policy is a legally binding agreement between the
+            Company and the Partner (the &#8220;Partner&#8221;). By accessing or
+            using our Services, the Partner agrees to abide by and not derogate
+            from the terms of this Privacy Policy, as amended from time to time,
+            and consent to the collection and use of information in the manner
+            set out in this Privacy Policy. We encourage our Partner to
+            periodically review this Privacy Policy.
+          </p>
+          <p>
+            The definitions in the Terms of Service apply to this Privacy Policy
+            unless stated otherwise. In addition to this policy, please review
+            our Terms of Service, which is incorporated herein by reference,
+            together with such other policies of which the Partner may be
+            notified of by the Company from time to time.
+          </p>
+          <h1 className="western">1. What information we collect</h1>
+          <p>
+            We only need to collect minimal information for advertising
+            purposes. We collect the following types of data:
+          </p>
+          <h2 className="western">1.1 Anonymous Information</h2>
+          <p>
+            The anonymous information the Company collects (i.e. information
+            that does not enable identification of individual persons) includes
+            technical information transmitted by the end user’s device,
+            including the end user’s software specifications (e.g. browser type
+            and version, operating system, version).
+          </p>
+          <h2 className="western">1.2 Personal Information</h2>
+          <p>
+            We only collect the Android Advertising Identifier, needed to
+            display advertising by third parties. We do not collect any other
+            personal information such as name, passwords.
+          </p>
+          <h2 className="western">1.2.1 Advertising Identifiers</h2>
+          <p>
+            When you are using our a mobile application that incorporates our
+            Services, we may also automatically record your Android Advertising
+            ID (if you are using an Android device) , for advertising or
+            analytics purposes. The Google Advertising ID is an anonymous
+            identifier, provided by Google Play services. If your device has an
+            Advertising ID, we may collect and use it for advertising and user
+            analytics purposes. If your device does not have Advertising ID, we
+            may use other persistent identifiers. The information collected may
+            be also stored on your device. Please see below how you can reset
+            you mobile Advertising ID or opt-out of receiving targeted ads
+            through your mobile Advertising IDs.
+          </p>
+          <h1 className="western">
+            2. What is the Purpose of collecting the information?
+          </h1>
+          <p>We collect the information specified above in order to:</p>
+          <p>
+            &#8211; enable the Company to further provide, develop, enhance,
+            customize and improve the Services, based on the end users&#8217;
+            preferences, uses and interactions with the Services;
+          </p>
+          <p>
+            &#8211; detect, prevent, or otherwise address fraud, security or
+            technical issues;
+          </p>
+          <p>
+            &#8211; to respond to claims that any content available through the
+            Service violates rights of third-parties;
+          </p>
+          <p>
+            To resolve disputes and enforce our policies, including
+            investigation of potential violations thereof, for the purpose of
+            law enforcement or in accordance with any applicable law or
+            regulation.
+          </p>
+          <h1 className="western">3. Mobile Device Opt-Out</h1>
+          <p>
+            You may opt out of receiving targeted ads on mobile devices using
+            the Android operating system, by using platform level controls
+            enabled through your device settings. You should consult your device
+            settings and instructions provided by your device manufacturer.
+            However, we provide the following information solely for
+            informational purposes.
+          </p>
+          <p>
+            If you have an Apple device, you can opt out of targeted advertising
+            by updating to iOS 6.0 or higher, and setting &#8220;Limit Ad
+            Tracking&#8221; to &#8220;ON&#8221;. You can do this by clicking on
+            Settings -&gt; Privacy -&gt; Advertising, and toggling &#8220;Limit
+            Ad Tracking&#8221; to &#8220;ON&#8221;. This prevents apps that you
+            use from using your Apple device’s Advertising Identifier to serve
+            you targeted ads. In addition, or as an alternative, you may reset
+            your device’s Advertising Identifier, which will result in your
+            device being disassociated from ad targeting profiles based on the
+            Advertising Identifier in effect prior to reset. You can do this by
+            clicking on Settings -&gt; Privacy -&gt; Advertising -&gt; Reset
+            Advertising Identifier. Again, you should consult Apple’s support
+            web pages and instructions for the most current applicable settings.
+          </p>
+          <p>
+            If you have an Android device, you can opt out of targeted
+            advertising on your device. You can do this by clicking the menu
+            icon to display the apps list, then find and select the Google
+            Settings icon, then find and select &#8220;Ads&#8221; and check the
+            box &#8220;Opt out of interest-based ads&#8221;. In addition, or as
+            an alternative, you may reset your device’s Android advertising ID,
+            which will result in your device being disassociated from ad
+            targeting profiles based on the Android advertising ID in effect
+            prior to reset. You can do this by clicking the menu icon to display
+            the apps list, then find and select the Google Settings icon, then
+            find and select &#8220;Ads&#8221; and selecting &#8220;Reset
+            advertising ID&#8221;. Note that Android settings menus may vary
+            among device manufacturers and these instructions may not be
+            accurate for your device. You should consult Google’s settings and
+            instructions for the most current settings information.
+          </p>
+          <h1 className="western">4. With whom we share information</h1>
+          <p>
+            We do not share Personal Information with third parties except as
+            specifically consented by the end user pursuant to this Privacy
+            Policy (for advertising purposes) or if required by applicable laws
+            or court order.
+          </p>
+          <p>
+            Anonymous information may be shared with other third parties (ad
+            networks, advertising companies, service providers, media) for the
+            purpose of developing or delivering advertising.
+          </p>
+          <h1 className="western">5. Children&#8217;s information</h1>
+          <p>
+            The Services are not directed or intended for children under 13
+            years of age. We do not knowingly collect or solicit information
+            from individuals under 13 years of age. If we later obtain actual
+            knowledge that an end user is under 13 years of age, we will take
+            steps to remove that end user&#8217;s information from our systems.
+          </p>
+          <h1 className="western">6. Storing and retaining the information</h1>
+          <p>
+            We do not store the information collected on any of our servers but
+            collect and deliver it to third parties only for advertising
+            purposes.
+          </p>
+          <h1 className="western">7. Security and confidentiality</h1>
+          <p>
+            We use industry standard information security tools and measures, as
+            well as internal procedures and strict guidelines to prevent
+            information misuse and data leakage. Our team accesses the
+            information on a &#8220;need-to-know&#8221; basis and subject to
+            confidentiality obligations.
+          </p>
+          <p>
+            While we strive to use commercially acceptable means and procedures
+            to protect the information, which considerably reduce the risks of
+            data misuse, we cannot guarantee that our systems will be absolutely
+            safe. To notify the Company about any security vulnerability or
+            potential data breach, please contact us at: citybeetles@gmail.com,
+            and we will take the appropriate measures to address such incident,
+            as deemed necessary.
+          </p>
+          <h1 className="western">8. Merger, sale or bankruptcy</h1>
+          <p>
+            In the event that the Company is acquired by or merged with a third
+            party entity, or other corporate transaction, or in the event of
+            bankruptcy or a comparable event, we reserve the right to transfer
+            or assign information in connection with the foregoing events. In
+            the event of the above, our affiliated companies or acquiring
+            company will assume the rights and obligations as described in this
+            Privacy Policy.
+          </p>
+          <h1 className="western">
+            9. Updates or amendments to the Privacy Policy
+          </h1>
+          <p>
+            We reserve the right to periodically amend or revise the Privacy
+            Policy; material changes will be effective immediately upon such
+            changes being presented on our Privacy Policy. The continued use of
+            the Services, following notice of such amendments, constitutes
+            acknowledgement and consent of such amendments to the Privacy Policy
+            and agreement to be bound by the terms of such amendments. The last
+            revision will be reflected in the &#8220;Last Updated&#8221;
+            heading.
+          </p>
+          <h1 className="western">Contact Us</h1>
+          <p>
+            For further information or queries please contact us at
+            contact@haidraco-labs.com.
+          </p>
+        </div>
+    </Container>
+  );
+}
